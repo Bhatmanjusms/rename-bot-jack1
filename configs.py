f01429052afedf1065cdb07f17b25ae675119249
Manjunath Bhat <86521081+Bhatmanjusms@users.noreply.github.com>
2022-12-13 17:58:46 +0530

    Update configs.py

--- configs.py
+++ configs.py
@@ -13,16 +13,16 @@ logging.basicConfig(
 from dotenv import load_dotenv
 load_dotenv()
 class Config(object):
-    API_ID = int(os.environ.get("API_ID", "8813038"))
-    API_HASH = os.environ.get("API_HASH", "780fd96b159baa710dada78ff1621b54")
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "5773205742:AAFSygtZMc_9ow_miS4sWmn5Rw0vQZMdoOc")
+    API_ID = int(os.environ.get("API_ID", "977080"))
+    API_HASH = os.environ.get("API_HASH", "0c20c4265501492a1513f91755acd42b")
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "5917502835:AAEMciruFROitEM9ty-K-VeRMiGGjYZQdVk")
     DOWNLOAD_DIR = os.environ.get("DOWNLOAD_DIR", "./downloads")
     LOGGER = logging
-    OWNER_ID = int(os.environ.get("OWNER_ID", 2083503061))
+    OWNER_ID = int(os.environ.get("OWNER_ID", 399726799))
     PRO_USERS = list({int(x) for x in os.environ.get("PRO_USERS", "0").split()})
     PRO_USERS.append(OWNER_ID)
     MONGODB_URI = os.environ.get("MONGODB_URI", "mongodb+srv://abcd:abcd@cluster0.od5wfzt.mongodb.net/?retryWrites=true&w=majority")
-    LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1001884096843"))
+    LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1001896983424"))
     BROADCAST_AS_COPY = bool(os.environ.get("BROADCAST_AS_COPY", "False"))
     FROM_CHANNEL = int(os.environ.get("FROM_CHANNEL", "-1001880691367"))
     TO_CHANNEL = int(os.environ.get("TO_CHANNEL", "-1001785157194"))
